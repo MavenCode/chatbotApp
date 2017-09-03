8d6dc77430e4a02eef65547ab9a953b2a00bdbac
Taiwo O. Adetiloye <taiwo.adetiloye@gmail.com>
2017-09-03 04:24:11 -0400

    add refresh icon

--- chatbotApp/uploads/core/views.py
+++ chatbotApp/uploads/core/views.py
@@ -13,16 +13,7 @@ def home(request):
     return render(request, 'core/home.html')
 
 
-def simple_upload(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'core/simple_upload.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
-    return render(request, 'core/simple_upload.html')
+
 
 
 def model_form_upload(request):
