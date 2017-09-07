02ab4b92be435e48ae2c251fdb7805294bd3142e
Taiwo O. Adetiloye <taiwo.adetiloye@gmail.com>
2017-09-07 04:37:02 -0400

    Procfile

--- uploads/urls.py
+++ uploads/urls.py
@@ -1,16 +1,25 @@
-from django.conf.urls import url
+from django.conf.urls import include,url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from chatterbot.ext.django_chatterbot import urls as chatterbot_urls
+
 
 from uploads.core import views
 
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
-    url(r'^uploads/form/$', views.model_form_upload, name='model_form_upload'),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^uploads/form/$', views.model_form_upload, name='model_form_upload'),
+    url(r'^sample/questions/$', views.sample_questions, name='samples'),
+    url(r'^api/chatterbot/', include(chatterbot_urls, namespace='chatterbot')),
+    url(r'^', include('send_email.urls')), #credit: https://wsvincent.com/django-contact-form/
+    
+
+    
+
+    
 ]
 
 if settings.DEBUG:
