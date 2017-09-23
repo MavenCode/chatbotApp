59510c4381fd5927ca9c23f93e039bce6e403b20
Taiwo O. Adetiloye <taiwo.adetiloye@gmail.com>
2017-09-23 06:23:23 -0400

    modified Allowed_host

--- uploads/settings.py
+++ uploads/settings.py
@@ -235,8 +235,8 @@ STATICFILES_DIRS = (
                 # Extra places for collectstatic to find static files.
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
