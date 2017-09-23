6b67b3054ba78036ac6887635ccac4aed80c7daa
Taiwo O. Adetiloye <taiwo.adetiloye@gmail.com>
2017-09-23 09:51:16 -0400

    modified Allowed_host

--- uploads/settings.py
+++ uploads/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
 
     'chatterbot.ext.django_chatterbot',
     'uploads.core',
