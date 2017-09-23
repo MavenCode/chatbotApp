808a5eb005b2ecc38c5cb6768412fbae8afb69cc
Taiwo O. Adetiloye <taiwo.adetiloye@gmail.com>
2017-09-23 05:46:20 -0400

    modified Allowed_host

--- uploads/settings.py
+++ uploads/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'e#-^aknk(5k)ej6rh#h$i(%h(m9)-j
 #DEBUG = False
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', False )
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = ['adverto.ai.herokudns.com', 'www.adverto.ai.herokudns.com','199.73.55.45']
 
 SECURE_SSL_REDIRECT = False
 # Application definition
